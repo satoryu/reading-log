842b2fa5af3a7462553f763066faeb7f5d0906b3
Tatsuya Sato <satoryu.1981@gmail.com>
2025-11-11 14:35:51 +0900

    Configures ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,47 @@
+// eslint.config.js
 import js from '@eslint/js'
-import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  js.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: false, // 型チェックなしで軽く始める
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        localStorage: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      prettier,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+      'react/react-in-jsx-scope': 'off', // Vite+Reactでは不要
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
-])
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+]
