bad031e88ee0180c18df11e91e11aca7eea259cd
Tatsuya Sato <satoryu.1981@gmail.com>
2025-11-15 16:47:21 +0900

    Add cloning function to storage.ts for safety

--- src/storage.ts
+++ src/storage.ts
@@ -4,14 +4,18 @@ export const load = <T>(key: string, fallback: T): T => {
   const value = storage[key]
 
   if (value === undefined) {
-    return fallback
+    return clone(fallback)
   }
 
-  return value as T
+  return clone(value as T)
+}
+
+const clone = <T>(value: T): T => {
+  return JSON.parse(JSON.stringify(value)) as T
 }
 
 export const save = <T>(key: string, value: T): void => {
-  storage[key] = value
+  storage[key] = clone(value)
 }
 
 export const clear = (key: string) => {
